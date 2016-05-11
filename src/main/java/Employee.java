e12260d10d8c06f379e1c637c9d18322488b9497
chandani.thacker <chandani.thacker@attuneww.com>
2016-05-11 18:12:22 +0530

    Initial setup

---
+++ src/main/java/Employee.java
@@ -0,0 +1,9 @@
+
+public class Employee 
+{
+	public int getSalary(int numofDays,int dailySalary)
+	{
+		return numofDays*dailySalary;
+	}
+
+}
---
+++ src/test/java/TesterForStudent.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TesterForStudent 
+{
+	
+	Student obj=new Student();
+	@Test
+	public void testCountAvg() 
+	{
+		int res=obj.countAvg(10, 20, 30);
+		Assert.assertEquals(res, 20);
+	}
+
+	@Test
+	public void testGetGrades() {
+		String res=obj.getGrades(95);
+		Assert.assertEquals("A", res);
+	}
+
+}
